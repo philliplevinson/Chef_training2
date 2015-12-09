5e7ade98b22072cc881b7c918b5cdcc2b0d5c0c5
ChefDK User <chef@ip-172-31-7-157.ec2.internal>
2015-12-09 20:20:02 +0000

    Version 0.2.0 - added Node Details in MOTD

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,6 +5,16 @@ package 'git'
 package 'emacs'
 
 file '/etc/motd' do
-	content 'This server is property of QuestDiagnostics all usage will be monitored'
+	content "This server is property of QuestDiagnostics all usage will be monitored
+
+	
+	IPADDRESS: #{node['ipaddress']}
+	HOSTNAME : #{node['hostname']}
+	Memory   : #{node['memory']['total']}
+	CPU      : #{node['cpu']['0']['mhz']}
+	"
+	mode '0644'
+	owner 'root'
+	group 'root'
 	action :create
 end
