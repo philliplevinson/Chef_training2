07291d6de137dcbfb65e60a9538eea42e9a5651f
ChefDK User <chef@ip-172-31-7-157.ec2.internal>
2015-12-10 15:04:39 +0000

    Changed file resource to template resource and defined a template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -4,17 +4,9 @@ package 'vim'
 package 'git'
 package 'emacs'
 
-file '/etc/motd' do
-	content "This server is property of QuestDiagnostics all usage will be monitored
-
-	
-	IPADDRESS: #{node['ipaddress']}
-	HOSTNAME : #{node['hostname']}
-	Memory   : #{node['memory']['total']}
-	CPU      : #{node['cpu']['0']['mhz']}
-	"
+template '/etc/motd' do
+	source 'motd.erb'
 	mode '0644'
 	owner 'root'
 	group 'root'
-	action :create
 end
